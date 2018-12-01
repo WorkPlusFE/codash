6f6976e90475512394deb9af039c0ba005e5698a
Hejx <531601727@qq.com>
2018-12-02 00:13:57 +0800

    Updates

---
+++ precache-manifest.d5e82430f6f5c5f6391fca7a3fb42a00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21ae07675b97f9cd69eaee5fa557a03",
+    "url": "/cordova-example/static/media/workplus.a21ae076.png"
+  },
+  {
+    "revision": "ea701d17a7446effc3b8",
+    "url": "/cordova-example/static/js/runtime~main.ea701d17.js"
+  },
+  {
+    "revision": "48e1a6b554df73d59598",
+    "url": "/cordova-example/static/js/main.48e1a6b5.chunk.js"
+  },
+  {
+    "revision": "e2922961ba92487223aa",
+    "url": "/cordova-example/static/js/1.e2922961.chunk.js"
+  },
+  {
+    "revision": "48e1a6b554df73d59598",
+    "url": "/cordova-example/static/css/main.41506d92.chunk.css"
+  },
+  {
+    "revision": "e2922961ba92487223aa",
+    "url": "/cordova-example/static/css/1.c92bd95a.chunk.css"
+  },
+  {
+    "revision": "9326e8befc2612359517356289b18862",
+    "url": "/cordova-example/index.html"
+  }
+];
